97afd607e240d6a7f960e2e710f522cab3e66ea6
mediazjr <91097193+mediazjr@users.noreply.github.com>
2021-11-24 20:18:56 -0800

    added save button functionality to save and read info from local storage

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 3rd-Party-APIs_Work-Day-Scheduler_hw5
+
+## Description:
+
+## Screenshot:
+
+## Deployed Application:
+[Work Day Scheduler](https://mediazjr.github.io/3rd-Party-APIs_Work-Day-Scheduler_hw5/)
--- assests/js/script.js
+++ assests/js/script.js
@@ -1,24 +1,49 @@
-// function to check the time and display appropriate color 
-function timeCheck() {
-    var currentHour = moment().hours();
+// add save button functionality and save value to local storage
+$(document).ready(function() {
+    $('.saveBtn').on('click', function() {
+        var value = $(this).siblings(".description").val()
+        var time = $(this).parent().attr("id")
 
-    $('.time-block').each(function() {
-        var hourBlock = parseInt($(this).attr("id").split("-")[1]);
-        if (hourBlock < currentHour) {
-            $(this).addClass("past")
-        } else if (hourBlock === currentHour) {
-            $(this).removeClass('past');
-            $(this).addClass("present");
-        } else {
-            $(this).removeClass('past');
-            $(this).removeClass('present');
-            $(this).addClass("future");
-        }
-    })
-}
+        console.log(value, time);
+        localStorage.setItem(time, value);
+    });
 
-timeCheck();
 
-// variable that checks time every 15 seconds and writes that time to page
-var timeInterval = setInterval(timeCheck, 15000);
-$("#currentDay").text(moment().format('dddd, MMM Do hh:mm a'));
+    // function to check the time and display appropriate color 
+    function timeCheck() {
+        var currentHour = moment().hours();
+
+        $('.time-block').each(function() {
+            var hourBlock = parseInt($(this).attr("id").split("-")[1]);
+            if (hourBlock < currentHour) {
+                $(this).addClass("past")
+            } else if (hourBlock === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    timeCheck();
+
+    // variable that checks day and time every 15 seconds and writes to page
+    var timeInterval = setInterval(timeCheck, 15000);
+
+    // moment.js date format 
+    $("#currentDay").text(moment().format('dddd, MMM Do hh:mm a'));
+
+    // reads info stored in local storage and shows value on page 
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+})
